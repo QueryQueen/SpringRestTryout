2e55388876a3fc2384f3fd50b801e99931c14187
QueryQueen <Joedid@gmail.com>
2016-09-19 13:56:32 +0200

    first succesfull build. Manually added imports

--- src/main/java/nl/programit/people/domain/Person.java
+++ src/main/java/nl/programit/people/domain/Person.java
@@ -2,6 +2,12 @@ package nl.programit.people.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
 @Entity
 public class Person implements Serializable {
 
