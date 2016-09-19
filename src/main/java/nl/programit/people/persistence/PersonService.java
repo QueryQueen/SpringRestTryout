989f405a7125ba6a54f1beedff1e907cd59f0cd4
QueryQueen <Joedid@gmail.com>
2016-09-19 14:18:28 +0200

    added personrepository to personservice

--- src/main/java/nl/programit/people/persistence/PersonService.java
+++ src/main/java/nl/programit/people/persistence/PersonService.java
@@ -1,10 +1,16 @@
 package nl.programit.people.persistence;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 public class PersonService {
+	@Autowired
+	private PersonRepository personRepository;
 	
+	public void save(Person person){
+		this.personRepository.save(person;)
+	}
 }
